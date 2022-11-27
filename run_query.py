1a1ce02a76a57e40044ab5d0b37c545c1a0ff777
luca-vdb <luca.von.der.brelie@gmail.com>
2022-11-27 12:57:03 +0000

    x

--- run_query.py
+++ run_query.py
@@ -271,7 +271,7 @@ def main():
     if args.load_model_path is not None:
         logger.info("reload model from {}".format(args.load_model_path))
 
-        for key in list(model.state_dict().keys()):
+        for key in list(model.state_dict.keys()):
             model.state_dict[key.replace('bert', 'encoder')] = model.state_dict.pop(key)
 
         model.load_state_dict(torch.load(args.load_model_path))  # Removed  strict=False, need to fix this!
